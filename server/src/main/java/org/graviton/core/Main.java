287cb9175647e1d3bb7274b6dba8242118af8563
Botan <ahmed.botan94@gmail.com>
2016-10-29 03:17:26 +0200

    Header build

--- server/src/main/java/org/graviton/core/Main.java
+++ server/src/main/java/org/graviton/core/Main.java
@@ -41,7 +41,7 @@ public class Main {
         /* This will return Long.MAX_VALUE if there is no preset limit */
         long maxMemory = Runtime.getRuntime().maxMemory();
         /* Maximum amount of memory the JVM will attempt to use */
-        System.out.println("Maximum memory : " + (maxMemory == Long.MAX_VALUE ? "no limit" : maxMemory / 1000 + " MB"));
+        System.out.println("Maximum memory : " + (maxMemory == Long.MAX_VALUE ? "unlimited" : maxMemory / 1000 + " MB"));
 
         /* Total memory currently available to the JVM */
         System.out.println("Total memory available to JVM : " + Runtime.getRuntime().totalMemory() / 1000 + " MB");
